deff1302144cbc28aa99d066c7d8d723df2f757f
Teo G <thodorisgoul@gmail.com>
2026-02-04 18:15:53 +0200

    s

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,4 @@ const isLocal = window.location.hostname === "localhost" || window.location.host
 
 export const API_URL = isLocal 
     ? 'http://localhost:5000/api'  // Local Development URL
-    : 'https://customs-api.fly.dev'; // Production URL
+    : 'https://customs-api.fly.dev/api'; // Production URL
